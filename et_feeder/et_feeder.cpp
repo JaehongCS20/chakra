f571a14739696f276b34cdca38af07f6ecb67df2
JaehongCS20 <jaehongcho@kaist.ac.kr>
2024-04-25 08:41:17 +0000

    Support of GPT3 175B

--- et_feeder/et_feeder.cpp
+++ et_feeder/et_feeder.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace Chakra;
 
 ETFeeder::ETFeeder(string filename)
-  : trace_(filename), window_size_(4096), et_complete_(false) {
+  : trace_(filename), window_size_(10000000), et_complete_(false) {
   readNextWindow();
 }
 
